adbea66e83d3839337cbebe971e3f54f0de6ec40
Bob Harris <rsharris@bx.psu.edu>
2023-03-02 18:52:05 -0500

    .

--- tabular_tools/cigar.py
+++ tabular_tools/cigar.py
@@ -362,7 +362,7 @@ def trace_cigar_path(cigarInfo,targetStart=0,queryStart=0,reverseQuery=False):
 	if (reverseQuery): 
 		matchStep  = (1,-1)
 		insertStep = (0,-1)
-		deleteStep = (-1,0)
+		deleteStep = (1,0)
 	else:
 		matchStep  = (1,1)
 		insertStep = (0,1)
